7c1205cc19c683a2f71e175c123599c791391fdf
Viktoriya Sirotenko <viktoriya.sirotenko@gmail.com>
2020-05-31 10:13:32 +0300

    done

---
+++ homework-05/task-4.js
@@ -0,0 +1,38 @@
+"use strict";
+
+class StringBuilder {
+  constructor(str) {
+    this._value = str;
+  }
+
+  get value() {
+    return this._value;
+  }
+  set value(value) {
+    this._value = value;
+  }
+
+  append(str) {
+    this.value += str;
+  }
+
+  prepend(str) {
+    this.value = str + this.value;
+  }
+
+  pad(str) {
+    this.value += str;
+    this.value = str + this.value;
+  }
+}
+
+const builder = new StringBuilder(".");
+
+builder.append("^");
+console.log(builder.value); // '.^'
+
+builder.prepend("^");
+console.log(builder.value); // '^.^'
+
+builder.pad("=");
+console.log(builder.value); // '=^.^='
